fbf65681b3fe2f14a3347ddf4fa0eafec97958af
Neto test <zilya.zakirova1991@gmail.com>
2023-04-22 01:29:09 +0300

    Tests!

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -2,18 +2,23 @@ package ru.netology;
 
 public class Radio {
 
-    private int volume;
-    private int channelCount;// = 10;
-    private int maxChannel;// = 9;
-    private int minChannel;// = 0;
-    private int currentChannelNumber=0;//minChannel;
+    private int minVolume=0;
+    private int maxVolume=100;
+    private int currentVolume=minVolume;
+    private int channelCount= 10;
+    private int maxChannel= 9;
+    private int minChannel=0;
+    private int currentChannelNumber=minChannel;
 
-    public Radio(int channelCount, int maxChannel, int minChannel, int currentChannelNumber, int volume) {
+    public Radio(int channelCount, int maxChannel, int minChannel, int currentChannelNumber, int minVolume,int maxVolume,int currentVolume) {
         this.channelCount = channelCount;
         this.maxChannel = maxChannel;
         this.minChannel = minChannel;
         this.currentChannelNumber = currentChannelNumber;
-        this.volume = volume;
+        this.minVolume = minVolume;
+        this.maxVolume=maxVolume;
+        this.currentVolume=currentVolume;
+
     }
 
     public Radio() {
@@ -22,7 +27,7 @@ public class Radio {
 
     public void setCurrentChannelNumber(int newChannelNumber) {
         if (newChannelNumber < minChannel) {
-            return;
+            return ;
         }
         if (newChannelNumber > maxChannel) {
             return;
@@ -30,24 +35,22 @@ public class Radio {
         currentChannelNumber = newChannelNumber;
     }
 
-    public int getCurrentChannelNumber() {
-        return currentChannelNumber;
-    }
+   public int getCurrentChannelNumber(){return currentChannelNumber;}
 
     public void setVolume(int newVolume) {
         if (newVolume < 0) {
-            volume = 0;
+            currentVolume = 0;
             return;
         }
         if (newVolume > 100) {
-            volume = 100;
+            currentVolume = 100;
             return;
         }
-        volume = newVolume;
+        currentVolume = newVolume;
     }
 
     public int getVolume() {
-        return volume;
+        return currentVolume;
     }
 
     public void next() {
@@ -68,16 +71,15 @@ public class Radio {
 
 
     public void increaseVolume() {
-        if (volume < 100) {
-            volume = volume + 1;
-        } else
-        volume=100;
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        } else currentVolume=100;
     }
 
     public void decreaseVolume() {
-        if (volume > 0) {
-            volume = volume - 1;
-        } else volume=0;
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        } else currentVolume=0;
     }
 }
 
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -6,40 +6,40 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
     @Test
     public void shouldChangeChannelToNext() {
-        Radio rad = new Radio(10, 9, 0, 7, 50);
+        Radio rad = new Radio(10, 9, 0, 7, 0,100,50);
         rad.next();
         Assertions.assertEquals(8, rad.getCurrentChannelNumber());
     }
 
     @Test
     public void shouldChangeChannelToNextIf9() {
-        Radio rad = new Radio(10, 9, 0, 9, 50);
+        Radio rad = new Radio(10, 9, 0, 9,0,100, 50);
         rad.next();
         Assertions.assertEquals(0, rad.getCurrentChannelNumber());
     }
 
     @Test
     public void shouldChangeChannelToNextIfMinus() {
-        Radio rad = new Radio(10, 9, 0, -12, 50);
+        Radio rad = new Radio(10, 9, 0, -12,0,100, 50);
         Assertions.assertEquals(0, rad.getCurrentChannelNumber());
     }
 
     @Test
     public void shouldChangeChannelToNextIfPlus() {
-        Radio rad = new Radio(10, 9, 0, 65, 50);
+        Radio rad = new Radio(10, 9, 0, 65,0,100, 50);
         Assertions.assertEquals(0, rad.getCurrentChannelNumber());
     }
 
     @Test
     public void shouldChangeChannelToPrevIf0() {
-        Radio rad = new Radio(10, 9, 0, 0, 50);
+        Radio rad = new Radio(10, 9, 0, 0,0,100, 50);
         rad.prev();
         Assertions.assertEquals(9, rad.getCurrentChannelNumber());
     }
 
     @Test
     public void shouldChangeChannelToPrev() {
-        Radio rad = new Radio(10, 9, 0, 2, 50);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, 50);
         rad.prev();
         Assertions.assertEquals(1, rad.getCurrentChannelNumber());
     }
@@ -47,7 +47,7 @@ public class RadioTest {
 
     @Test
     public void shouldIncreaseVolume() {
-        Radio rad = new Radio(10, 9, 0, 2, 50);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, 50);
         rad.increaseVolume();
         Assertions.assertEquals(51, rad.getVolume());
 
@@ -55,7 +55,7 @@ public class RadioTest {
 
     @Test
     public void shouldIncreaseVolumeIf100() {
-        Radio rad = new Radio(10, 9, 0, 2, 100);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, 100);
         rad.increaseVolume();
         Assertions.assertEquals(100, rad.getVolume());
 
@@ -63,7 +63,7 @@ public class RadioTest {
 
     @Test
     public void shouldDecreaseVolume() {
-        Radio rad = new Radio(10, 9, 0, 2, 30);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, 30);
         rad.decreaseVolume();
         Assertions.assertEquals(29, rad.getVolume());
 
@@ -71,7 +71,7 @@ public class RadioTest {
 
     @Test
     public void shouldDecreaseVolumeIf0() {
-        Radio rad = new Radio(10, 9, 0, 2, 0);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, 0);
         rad.decreaseVolume();
         Assertions.assertEquals(0, rad.getVolume());
 
@@ -79,14 +79,14 @@ public class RadioTest {
 
     @Test
     public void shouldIncreaseVolumeToMax() {
-        Radio rad = new Radio(10, 9, 0, 2, 257);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, 257);
       //  rad.increaseVolume();
         Assertions.assertEquals(100, rad.getVolume());
     }
 
     @Test
     public void shouldIncreaseVolumeToMinus() {
-        Radio rad = new Radio(10, 9, 0, 2, -7);
+        Radio rad = new Radio(10, 9, 0, 2,0,100, -7);
         //rad.increaseVolume();
         Assertions.assertEquals(0, rad.getVolume());
     }
